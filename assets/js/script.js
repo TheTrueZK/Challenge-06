4ca571d5fd1576a4b16f167c59475737fd4e334b
TheTrueZK <thetruezk@gmail.com>
2022-07-05 11:06:15 -0500

    finished HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-var APIKey = "b65ff68781f3e2de7d7e0203b5401f45";
+var todayweatherEl = document.getElementById("today-weather");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+
+var weatherAPIKey = "b65ff68781f3e2de7d7e0203b5401f45";
+
+function getWeather(cityName) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    axios.get(queryURL)
+        .then(function (response) {
+
+            todayweatherEl.classList.remove("d-none");
+
+            var currentDate = new Date(response.data.dt * 1000);
+            var day = currentDate.getDate();
+            var month = currentDate.getMonth() + 1;
+            var year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            let weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt", response.data.weather[0].description);
+        })
+}
